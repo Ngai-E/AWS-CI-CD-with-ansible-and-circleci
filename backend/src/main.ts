721a011b625e72f0a07ff269d1cd28ce1622f9ef
E.Ngai <elizabeth.ngai@dme.systems>
2023-01-06 11:03:39 +0100

    fix backend build error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ConfigService } from './modules/config/config.service';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './modules/app/app.module';
-import { AppLogger } from './modules/app/app.logger';
+import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cors from 'cors';
-import { TransformInterceptor } from './modules/common/interceptors/TransformInterceptor';
 import * as express from 'express';
+import { AppLogger } from './modules/app/app.logger';
+import { AppModule } from './modules/app/app.module';
+import { TransformInterceptor } from './modules/common/interceptors/TransformInterceptor';
+import { ConfigService } from './modules/config/config.service';
 import { ErrorFilter } from './modules/errors/error.filter';
 
 async function bootstrap() {
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth()x // here is an intentional compile error. Remove the "x" and the backend should compile.
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
